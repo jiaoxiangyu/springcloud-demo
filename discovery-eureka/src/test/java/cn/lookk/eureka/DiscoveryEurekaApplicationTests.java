11043a82751b48f49a07733b48c987ab1259b23d
jiaoxiangyu <1062100557@qq.com>
2020-11-06 19:49:51 +0800

    init

---
+++ discovery-eureka/src/test/java/cn/lookk/eureka/DiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lookk.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
