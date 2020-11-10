67629699f8557cd17fb0c8fdd5e19ba8824d9545
jiaoxiangyu <1062100557@qq.com>
2020-11-10 18:54:04 +0800

    熔断器监控hystrix-dashboard 未完成

--- consumer/src/main/java/cn/lookk/consumer/ConsumerApplication.java
+++ consumer/src/main/java/cn/lookk/consumer/ConsumerApplication.java
@@ -3,11 +3,13 @@ package cn.lookk.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+//@EnableTurbine
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
