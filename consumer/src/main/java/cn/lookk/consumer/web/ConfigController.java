b21e35c3f921f6d1c9c31718e9bc12e254c677b1
jiaoxiangyu <1062100557@qq.com>
2020-11-12 18:55:19 +0800

    配置中心 config 未完成

---
+++ consumer/src/main/java/cn/lookk/consumer/web/ConfigController.java
@@ -0,0 +1,36 @@
+package cn.lookk.consumer.web;
+
+import cn.lookk.handleexception.util.ResultUtil;
+import cn.lookk.handleexception.vo.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: ConfigController
+ * @Description: TODO
+ * @Author jiaoxiangyu
+ * @Date 2020/11/12
+ * @Version 1.0
+ * @Since JDK1.8
+ */
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigController.class);
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/info")
+    public Result findByName(){
+
+        return ResultUtil.success(configInfo);
+    }
+}
