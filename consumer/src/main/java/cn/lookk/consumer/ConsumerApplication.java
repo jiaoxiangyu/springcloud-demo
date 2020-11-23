65f78096ae04a23654daea4dcfb25c14b8fc928c
jiaoxiangyu <jxy19995824>
2020-11-24 00:02:38 +0800

    配置中心-config: 解决本地配置文件找不到配置项报错

--- consumer/src/main/java/cn/lookk/consumer/ConsumerApplication.java
+++ consumer/src/main/java/cn/lookk/consumer/ConsumerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @ComponentScan("cn.lookk.*")
@@ -20,4 +22,13 @@ public class ConsumerApplication {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
+	//解决本地配置文件找不到配置项报错
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+		c.setIgnoreUnresolvablePlaceholders(true);
+
+		return c;
+
+	}
 }
