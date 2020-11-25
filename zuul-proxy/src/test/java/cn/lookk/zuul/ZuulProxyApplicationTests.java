8e9a045450b4d5df202b4d600cbad50f653c9bfa
jiaoxiangyu <jxy19995824>
2020-11-25 23:51:35 +0800

    路由：zuul

---
+++ zuul-proxy/src/test/java/cn/lookk/zuul/ZuulProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lookk.zuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
