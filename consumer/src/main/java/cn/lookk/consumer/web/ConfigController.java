bcd4828d105b8ec7a107c6ac38b55bce78dbab3a
jiaoxiangyu <1062100557@qq.com>
2020-11-25 21:42:45 +0800

    配置中心 config refresh

--- consumer/src/main/java/cn/lookk/consumer/web/ConfigController.java
+++ consumer/src/main/java/cn/lookk/consumer/web/ConfigController.java
@@ -5,6 +5,7 @@ import cn.lookk.handleexception.vo.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  * @Since JDK1.8
  */
+@RefreshScope
 @RestController
 @RequestMapping("/config")
 public class ConfigController {
@@ -30,7 +32,7 @@ public class ConfigController {
 
     @GetMapping("/info")
     public Result findByName(){
-
+        logger.info("configInfo={}", configInfo);
         return ResultUtil.success(configInfo);
     }
 }
